4753a83d0b998540cd248c634540d469aee89850
jaggi1803 <88776121+jaggi1803@users.noreply.github.com>
2023-07-30 20:11:27 +0530

    Update index.js

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -9,12 +9,7 @@ function Profile() {
             <Tabs.TabPane tab="Products" key="1">
                 <Products />
             </Tabs.TabPane>
-            <Tabs.TabPane tab="Bids" key="2">
-                <h1>Bids</h1>
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="General" key="3">
-                <h1>General</h1>
-            </Tabs.TabPane>
+
         </Tabs>
     </div>
   )
